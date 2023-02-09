29be8b1e2d30b83ab69a30b7fd88786b989e18b4
Abidemi Ope <opeabidemi@gmail.com>
2023-02-09 12:07:00 +0100

    new lesson

---
+++ src/main/kotlin/lesson4/Main.kt
@@ -0,0 +1,22 @@
+package lesson4
+
+fun main() {
+    val name = "james bond"
+
+    println(name.capital(3))
+
+    val two = 2
+
+    println(two.isEven())
+}
+
+fun Int.isEven(): Boolean {
+    return this % 2 == 0
+}
+
+fun String.capital(index: Int): String {
+    val arr = this.toCharArray()
+    arr[index] = this[index].toUpperCase()
+
+    return  arr.joinToString("")
+}
